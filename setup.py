3d4cab1a3075864a727fb43f988881a97500f146
Eugene M. Kim <eugene.kim@ntti3.com>
2017-10-24 16:35:24 -0700

    Bump version: 0.1.2 → 0.1.3

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ test_requirements = [
 
 setup(
     name='pysrc3',
-    version='0.1.2',
+    version='0.1.3',
     description="Opens the source file of Python modules in an editor, "
                 "a pager, or any other program of choice.",
     long_description=readme + '\n\n' + history,
